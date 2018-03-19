cda654652446a9f28856dab98f13e9ce5fda4d84
Håkon Krogh <hakonhk@gmail.com>
2018-03-19 16:04:43 +0100

    Added classNames for all components

--- src/module/styles.js
+++ src/module/styles.js
@@ -34,14 +34,18 @@ export const Outer = styled.div.attrs({
   will-change: left, right;
 `;
 
-export const Content = styled.div`
+export const Content = styled.div.attrs({
+  className: 'crystallize-layout__content'
+})`
   position: relative;
   z-index: 2;
   overflow-x: ${getContentOverflowX};
   background: ${p => p.background};
 `;
 
-export const Left = styled.div`
+export const Left = styled.div.attrs({
+  className: 'crystallize-layout__menu crystallize-layout__menu--left'
+})`
   position: fixed;
   top: 0;
   left: ${getPosition};
@@ -55,15 +59,25 @@ export const Left = styled.div`
   will-change: left;
 `;
 
-export const Right = Left.extend`
-  left: auto;
+export const Right = styled.div.attrs({
+  className: 'crystallize-layout__menu crystallize-layout__menu--right'
+})`
+  position: fixed;
+  top: 0;
   right: ${getPosition};
-  width: ${p => p.width || '300px'};
+  z-index: 1;
+  height: 100%;
+  height: 100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
   transition: right 0.3s cubic-bezier(0.41, 0.03, 0, 0.96);
+  width: ${p => p.width || '300px'};
   will-change: right;
 `;
 
-export const ClickOverlay = styled.div`
+export const ClickOverlay = styled.div.attrs({
+  className: 'crystallize-layout__click-overlay'
+})`
   position: absolute;
   top: 0;
   left: 0;
