86deac15914721b7f7a7ba68dfaf2f68b1f74c48
Håkon Krogh <hakonhk@gmail.com>
2018-04-27 14:08:35 +0200

    Respects the width of the device

--- module/index.js
+++ module/index.js
@@ -3,7 +3,8 @@ import Emitter from 'tiny-emitter';
 
 import { Outer, Left, Right, Content, ClickOverlay } from './styles';
 
-const defaultWidth = '300px';
+const defaultWidth = '500px';
+const marginToTriggerWidthOverride = 40;
 
 export const emitter = new Emitter();
 const showStatus = {
@@ -63,17 +64,99 @@ export const hideRight = () => toggleRight(false);
 export default class CrystallizeLayout extends Component {
   state = {
     showLeft: showStatus.left,
-    showRight: showStatus.right
+    showRight: showStatus.right,
+    widthOverride: null
+  };
+
+  activeWidthOverrides = {
+    left: false,
+    right: false,
+    get any() {
+      return this.left || this.right;
+    }
   };
 
   componentDidMount() {
     emitter.on('toggle', this.onToggle);
     speed = parseInt(this.props.speed || 300);
+    this.listenForMediaChange();
   }
 
   componentWillUnmount() {
     emitter.off('toggle', this.onToggle);
+    this.stopListenForMediaChange();
   }
+
+  startWidthOverride = side => {
+    if (!this.activeWidthOverrides.any) {
+      this.handleWindowResize();
+      window.addEventListener('resize', this.handleWindowResize, false);
+    }
+
+    this.activeWidthOverrides[side] = true;
+  };
+
+  handleWindowResize = () => {
+    clearTimeout(this.widthOverrideSetTimeout);
+    this.widthOverrideSetTimeout = setTimeout(() => {
+      this.setState({
+        widthOverride: window.innerWidth - marginToTriggerWidthOverride + 'px'
+      });
+    }, 25);
+  };
+
+  stopWidthOverride = side => {
+    this.activeWidthOverrides[side] = false;
+
+    if (!this.activeWidthOverrides.any) {
+      window.removeEventListener('resize', this.handleWindowResize);
+      clearTimeout(this.widthOverrideSetTimeout);
+    }
+  };
+
+  listenForMediaChange = () => {
+    const { width = defaultWidth, leftWidth, rightWidth } = this.props;
+
+    const left = leftWidth || width;
+    const right = rightWidth || width;
+
+    this.leftMql = window.matchMedia(
+      `(min-width: ${parseInt(left, 10) + marginToTriggerWidthOverride}px)`
+    );
+    this.leftMql.addListener(this.handleLeftMediaChange);
+    this.handleLeftMediaChange(this.leftMql);
+
+    this.rightMql = window.matchMedia(
+      `(min-width: ${parseInt(right, 10) + marginToTriggerWidthOverride}px)`
+    );
+    this.rightMql.addListener(this.handleRightMediaChange);
+    this.handleRightMediaChange(this.rightMql);
+  };
+
+  handleLeftMediaChange = mql => {
+    const theScreenIsTooSmallForMenu = !mql.matches;
+    if (theScreenIsTooSmallForMenu) {
+      this.startWidthOverride('left');
+    } else {
+      this.stopWidthOverride('left');
+    }
+  };
+
+  handleRightMediaChange = mql => {
+    const theScreenIsTooSmallForMenu = !mql.matches;
+    if (theScreenIsTooSmallForMenu) {
+      this.startWidthOverride('right');
+    } else {
+      this.stopWidthOverride('right');
+    }
+  };
+
+  stopListenForMediaChange = () => {
+    this.leftMql.removeListener(this.handleLeftMediaChange);
+    this.rightMql.removeListener(this.handleRightMediaChange);
+    this.stopWidthOverride('left');
+    this.stopWidthOverride('right');
+  };
 
   disableScroll(stop) {
     if (!this.defaultBodyOverflow) {
@@ -116,9 +199,14 @@ export default class CrystallizeLayout extends Component {
       rightWidth,
       blurContentOnShow
     } = this.props;
+    const { widthOverride } = this.state;
 
-    const leftWidthToUse = leftWidth || width;
-    const rightWidthToUse = rightWidth || width;
+    const leftWidthToUse = this.activeWidthOverrides.left
+      ? widthOverride
+      : leftWidth || width;
+    const rightWidthToUse = this.activeWidthOverrides.right
+      ? widthOverride
+      : rightWidth || width;
 
     const { showLeft, showRight } = this.state;
 
